5a4884983d4d1a645cd043f0d32623c237e848b4
callysavio <callistusanwara@gmail.com>
2026-02-12 17:10:27 +0100

    fetch furniture and delete implemented

--- src/components/common/ProductsTable.jsx
+++ src/components/common/ProductsTable.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+/* eslint-disable react-hooks/set-state-in-effect */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { authUtils } from "../../utils/auth";
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
 
 function ProductsTable() {
+    const [furnitures, setFurniture] = useState([]);
+    const fetchAllFurniture = async () => {
+        const response = await axios.get(`${baseUrl}/api/furniture/all-furniture`);
+        const data = await response.data.data;
+        setFurniture(() => {
+            return data;
+        });
+    };
+    useEffect(() => {
+        fetchAllFurniture();
+    }, []);
+    //delete furniture
+    const deleteFurniture = async (id) => {
+        const res = await axios.delete(`${baseUrl}/furniture/delete-furniture/${id}`, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                ...authUtils.getAuthHeaders(),
+            },
+        })
+        res.data,
+            toast.success(res.data.message)
+    }
+
     return (
         <div class="overflow-x-auto">
             <table class="w-full text-left border-collapse">
                 <thead class="bg-gray-50 border-b">
                     <tr>
+                        <th class="p-4 font-medium text-gray-600">S/N</th>
                         <th class="p-4 font-medium text-gray-600">Product</th>
                         <th class="p-4 font-medium text-gray-600">Category</th>
                         <th class="p-4 font-medium text-gray-600">Price</th>
@@ -14,32 +44,55 @@ function ProductsTable() {
                         <th class="p-4 font-medium text-gray-600 text-right">Actions</th>
                     </tr>
                 </thead>
-                <tbody class="divide-y">
-                    <tr class="hover:bg-gray-50 transition">
-                        <td class="p-4 flex items-center gap-3">
-                            <div class="w-12 h-12 bg-gray-200 rounded-md overflow-hidden">
-                                <img src="/api/placeholder/100/100" alt="Sofa" class="object-cover w-full h-full" />
-                            </div>
-                            <div>
-                                <div class="font-medium text-gray-900">Velvet Armchair</div>
-                                <div class="text-xs text-gray-400">ID: #48291</div>
-                            </div>
-                        </td>
-                        <td class="p-4 text-sm text-gray-600">Living Room</td>
-                        <td class="p-4 text-sm font-semibold">$599.00</td>
-                        <td class="p-4 text-sm text-gray-600">24 pcs</td>
-                        <td class="p-4">
-                            <span class="px-2 py-1 text-xs font-medium bg-green-100 text-green-700 rounded-full">In Stock</span>
-                        </td>
-                        <td class="p-4 text-right">
-                            <button class="text-blue-600 hover:underline mr-3">Edit</button>
-                            <button class="text-red-500 hover:underline">Delete</button>
-                        </td>
-                    </tr>
-                </tbody>
+                {furnitures.map((furniture, index) => {
+                    return (
+                        <tbody class="divide-y">
+                            <tr key={furniture.id} class="hover:bg-gray-50 transition" >
+                                <td class="p-4 text-sm text-gray-600">{index + 1}</td>
+                                <td class="p-4 flex items-center gap-3">
+                                    <div class="w-12 h-12 bg-gray-200 rounded-md overflow-hidden">
+                                        <img
+                                            src={furniture.images[0]}
+                                            alt={furniture.name}
+                                            class="object-cover w-full h-full"
+                                        />
+                                    </div>
+                                    <div>
+                                        <div class="font-medium text-gray-900">
+                                            {furniture.name}
+                                        </div>
+                                        <div class="text-xs text-gray-400">ID: #48291</div>
+                                    </div>
+                                </td>
+                                <td class="p-4 text-sm text-gray-600">{furniture.category}</td>
+                                <td class="p-4 text-sm font-semibold">${furniture.price}</td>
+                                <td class="p-4 text-sm text-gray-600">
+                                    {furniture.quantity} pcs
+                                </td>
+                                <td class="p-4">
+                                    {furniture.inStock === true ? (
+                                        <span class="px-2 py-1 text-xs font-medium bg-green-100 text-green-700 rounded-full">
+                                            In Stock
+                                        </span>
+                                    ) : (
+                                        <span class="px-2 py-1 text-xs font-medium bg-red-700 text-white rounded-full">
+                                            Out of Stock
+                                        </span>
+                                    )}
+                                </td>
+                                <td class="p-4 text-right">
+                                    <button class="text-blue-600 hover:underline mr-3">
+                                        Edit
+                                    </button>
+                                    <button onClick={deleteFurniture(furniture._id)} class="text-red-500 hover:underline" > Delete</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    );
+                })}
             </table>
-        </div>
-    )
+        </div >
+    );
 }
 
-export default ProductsTable
+export default ProductsTable;
